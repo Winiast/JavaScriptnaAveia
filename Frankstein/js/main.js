9f8abeca264817cc0010fd7ff6c5238cba38abd0
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-01-26 09:52:56 -0300

    Validacao de login e usuarios com array

--- Frankstein/js/main.js
+++ Frankstein/js/main.js
@@ -1,5 +1,32 @@
 let login = document.querySelector("#send");
 
+function verificarLogin(name, password) {
+  let usuariosValidos = [
+    ["Gustavo", "123456"],
+    ["Jose", "123456"],
+  ];
+  for (i in usuariosValidos) {
+    let p = 1;
+    let y = 0;
+    if (password == usuariosValidos[i][p] && name == usuariosValidos[i][y]) {
+      console.log(usuariosValidos[i][y]);
+      console.log(usuariosValidos[i][p]);
+    }
+    y++;
+    p++;
+  }
+}
+
+function validacaoDeCampos(name, password) {
+  if (name == "") {
+    alert("Por favor preencha o campo Usuario");
+  } else if (password == "") {
+    alert("Por favor preencha o campo de Senha");
+  } else if (name != "" && password != "") {
+    verificarLogin(name, password);
+  }
+}
+
 login.addEventListener("click", function (evt) {
   evt.preventDefault();
 
@@ -7,13 +34,11 @@ login.addEventListener("click", function (evt) {
 
   const name = document.querySelector("#usuario");
 
-  const value = name.value;
+  const nameValue = name.value;
 
   const password = document.querySelector("#password");
 
   const passwordValue = password.value;
 
-  console.log(value);
-
-  console.log(password.value);
+  validacaoDeCampos(nameValue, passwordValue);
 });
